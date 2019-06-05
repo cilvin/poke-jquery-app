d260b75422a5f043863b0263c1a9f7a353405dc7
cilvin <cilvin33@users.noreply.github.com>
2019-06-05 11:48:32 -0700

    Delete $container variable

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var pokemonRepository = (function () {
 
     var repository = [];
     var apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
-    var $container = $('#exampleModal');
+   
     
 
     // Loading Data from an external api
