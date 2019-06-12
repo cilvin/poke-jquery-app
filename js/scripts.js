508ba828eac227df66a78084a63f8cf64e5c87b7
cilvin <cilvin33@users.noreply.github.com>
2019-06-12 16:03:12 -0700

    Change click event listener to be more specific
    Use id of pokemon button to add the event listener .click()

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 //Start of IIFE
+/* global $ */
+/*eslint no-console: ["error", { allow: ["error"] }] */
 var pokemonRepository = (function () {
 
     var repository = [];
     var apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
     var $Li = $('.btn-group-vertical');
+   
    
     function add(item) {
         repository.push(item)
@@ -18,7 +21,6 @@ var pokemonRepository = (function () {
     function loadList() {
         return $.ajax(apiUrl)
         .then(function(response) {
-            console.log('apiGet', response);
             response.results.forEach(function(item) {
                 var pokemon = {
                     name: item.name,
@@ -41,7 +43,6 @@ var pokemonRepository = (function () {
                 pokemon.imageUrl = response.sprites.front_default;
                 pokemon.height = response.height;
                 pokemon.weight = response.weight;
-                add(pokemon);
                 addListItem(pokemon);
             })
             .catch(function(e) {
@@ -53,7 +54,7 @@ var pokemonRepository = (function () {
     }
 
     function addListItem(pokemon) {
-        var $button = $(`<button
+    var $button = $(`<button
         id="boom"
         type="button"
         data-toggle="modal"
@@ -65,32 +66,8 @@ var pokemonRepository = (function () {
 
     }
 
-    function createModal() {
-        var $modal = $(`<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		<div class="modal-dialog" role="document">
-		  <div class="modal-content">
-			<div class="modal-header">
-			  <h5 class="modal-title" id="exampleModalLabel"></h5>
-			  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-				<span aria-hidden="true">&times;</span>
-			  </button>
-			</div>
-			<div class="modal-body">
-			</div>
-			<div class="modal-footer">
-			  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-			  <button type="button" class="btn btn-primary">Save changes</button>
-			</div>
-		  </div>
-		</div>
-		</div>`);
-
-    $Li.append($modal);
-    }
 
     function updateModal(pokemon) {
-        console.log('TCL: updateModal -> pokemon', pokemon);
-
         $Li.find('.modal-title').text(pokemon.name);
         $Li.find('.modal-body').html(`<div>
                                         <img src =${pokemon.imageUrl} />
@@ -109,7 +86,6 @@ var pokemonRepository = (function () {
         addListItem: addListItem,
         loadList: loadList,
         loadListDetails: loadListDetails,
-        createModal: createModal,
         updateModal: updateModal,
         
     };
@@ -121,17 +97,8 @@ var pokemonRepository = (function () {
 
 pokemonRepository.loadList().then(function(pokeArray) {
     //Create modal ONCE
-    pokemonRepository.createModal();
     pokemonRepository.loadListDetails(pokeArray);
 
-    $('html').click(function(event) {
-        console.log('TCL: addListItem -> event', event.target.innerText);
-        var findPokemon = pokeArray.find(function(p) {
-            return p.name === event.target.innerText;
-        });
-        console.log('TCL: findPokemon', findPokemon);
-        if (findPokemon) {
-            console.log('clicked');
-            pokemonRepository.updateModal(findPokemon);
-        }
+    $('#boom').click(function(event) {
+        pokemonRepository.updateModal(event);
     });});
